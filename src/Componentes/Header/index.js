2758b5f6a6340d5a514b48ecd1e8eb0e98be7034
gaboprada <gaboprada@gmai.com>
2025-01-06 23:40:54 -0500

    update imagen header

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function barraInicio (){
+    return
+}
+
+function Header () {
+    return (
+     <header><img className="img-header" src="/estudianteprogramacion.webp" alt="imagen header"/>
+     </header>);
+}
+
+export default Header
